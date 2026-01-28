3b671f03e0962bdfadb8581b0ff42ce9a3b46b1c
danielrfava <danielrfava@gmail.com>
2026-01-28 15:33:15 -0500

    Update [school].js

--- pages/teams/[school].js
+++ pages/teams/[school].js
@@ -1,4 +1,4 @@
-- import Nav from '../components/Nav';
-- import { getConfig } from '../lib/load';
+- import Nav from '../../components/Nav';
+- import { readJson } from '../../lib/load';
 + import Nav from '@/components/Nav';
-+ import { getConfig } from '@/lib/load';
++ import { readJson } from '@/lib/load';
