c019e2846c7b32123d6240c80b04276bc3025cc5
danielrfava <danielrfava@gmail.com>
2026-01-29 08:57:30 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,3 @@
-// pages/about.js
-import Nav from '@/components/Nav';
-
-export default function About() {
-  return (
-    <>
-      <Nav />
-      <div className="container">
-        <h1 className="h1">About • National Prep DeafSports (Division I)</h1>
-
-        <div className="card">
-          Curated, Deaf schools only analytics for the Big 6:
-          CSD Fremont, CSD Riverside, MSD, MSSD, TSD, ISD.
-          <br /><br />
-          This platform powers historical rankings, Clerc Classic history,
-          tournament results, and player search (coming soon).
-        </div>
-      </div>
-    </>
-  );
-}
+- import Nav from '@/components/Nav';
++ import Nav from '../components/Nav';
+``
