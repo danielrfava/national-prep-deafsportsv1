5685d8f4971a4c3036187091fb54454acc5dffd8
danielrfava <danielrfava@gmail.com>
2026-01-29 08:56:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-- import Nav from '../components/Nav';
-- import { getConfig } from '../lib/load';
-+ import Nav from '@/components/Nav';
-+ import { getConfig } from '@/lib/load';
+- import Nav from '@/components/Nav';
+- import { getConfig } from '@/lib/load';
++ import Nav from '../components/Nav';
++ import { getConfig } from '../lib/load';
