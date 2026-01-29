a844a83a7ac95321544cbdde5a8044ca44834019
danielrfava <danielrfava@gmail.com>
2026-01-29 08:58:05 -0500

    Update clerc.js

--- pages/clerc.js
+++ pages/clerc.js
@@ -1,5 +1,5 @@
-- import Nav from '../components/Nav';
-- import { getConfig } from '../lib/load';
-- import { readJson } from '../lib/load';
-+ import Nav from '@/components/Nav';
-+ import { getConfig, readJson } from '@/lib/load';
+- import Nav from '@/components/Nav';
+- import { getConfig } from '@/lib/load';
++ import Nav from '../components/Nav';
++ import { getConfig } from '../lib/load';
+``
